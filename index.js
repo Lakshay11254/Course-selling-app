2ff43d50a07574db0d78d5374999aed73cf01548
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-10-09 06:33:40 -0400

    Destructured the userRouter

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
     const express = require("express");
-
+    const {userRouter} = require("./routes/user");
     const app = express();
-
-   
-
+        app.use("/user", userRouter);
+        app.use("/course", courseRouter);
     
+
+        app.listen(3000);
