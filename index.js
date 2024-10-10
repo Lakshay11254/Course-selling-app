54e2657cd6edf933debd80f459106f2139495a7b
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-10-10 04:32:59 -0400

    admin

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
     const app = express();
 
         app.use("/api/v1/user", userRouter);
+        app.use("/api/v1/admin", adminRouter);
         app.use("/api/v1/course", courseRouter);
     
 
---
+++ routes/admin.js
@@ -0,0 +1,20 @@
+const {Router} = require("express");
+
+const adminRouter = Router();
+
+// adminRouter.use(addMiddleware);
+    adminRouter.post("/signup", function (req, res) {
+        res.json({
+            message: "courses endpoint"
+        })
+    })
+    adminRouter.post("/signin", function (req, res) {
+        res.json({
+            message: "courses endpoint"
+        })
+    })
+    adminRouter.post("/course", function (req, res) {
+        res.json({
+            message: "courses endpoint"
+        })
+    })
