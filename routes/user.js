4191367d0bae94f3019eddead4dc424826091853
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-10-11 03:45:35 -0400

    updated user.js

--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,46 @@
-const {Router} = require("express");
+const jwt = require("jsonwebtoken");
+const { JWT_USER_PASSWORD } = require("../config");
 
-const userRouter = Router();    
+function userMiddleware(req, res, next) {
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, JWT_USER_PASSWORD);
 
-    userRouter.post("/signup", function (req, res) {
-        res.json({
-            message: "signup endpoint"
+    if (decoded) {
+        req.userId = decoded.id;
+        next()
+    } else {
+        res.status(403).json({
+            message: "You are not signed in"
         })
-    })
+    }
 
-    userRouter.post("/signin", function (req, res) {
-        res.json({
-            message: "signin endpoint"
-        })
-    })
+}
 
-    userRouter.get("/purchases", function (req, res) {
-        res.json({
-            message: "purchases endpoint"
-        })
-    })
+module.exports = {
+    userMiddleware: userMiddleware
+}
+// const {Router} = require("express");
 
-    module.exports = {
-        userRouter: userRouter
-    }   
+// const userRouter = Router();    
+
+//     userRouter.post("/signup", function (req, res) {
+//         res.json({
+//             message: "signup endpoint"
+//         })
+//     })
+
+//     userRouter.post("/signin", function (req, res) {
+//         res.json({
+//             message: "signin endpoint"
+//         })
+//     })
+
+//     userRouter.get("/purchases", function (req, res) {
+//         res.json({
+//             message: "purchases endpoint"
+//         })
+//     })
+
+//     module.exports = {
+//         userRouter: userRouter
+//     }   
