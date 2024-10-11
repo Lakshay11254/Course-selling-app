42ba3e74ce5b250711f796cc86f1f40e44ebbcf4
Lakshay Malhotra <lakshay.sbp@gmail.com>
2024-10-11 03:43:47 -0400

    admin updated

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,61 @@
-const {Router} = require("express");
+const jwt = require("jsonwebtoken");
+const { JWT_ADMIN_PASSWORD } = require("../config");
 
-const adminRouter = Router();
+// function middleware(password) {
+//     return function(req, res, next) {
+//         const token = req.headers.token;
+//         const decoded = jwt.verify(token, password);
 
-// adminRouter.use(addMiddleware);
-    adminRouter.post("/signup", function (req, res) {
-        res.json({
-            message: "courses endpoint"
-        })
-    })
-    adminRouter.post("/signin", function (req, res) {
-        res.json({
-            message: "courses endpoint"
-        })
-    })
-    adminRouter.post("/course", function (req, res) {
-        res.json({
-            message: "courses endpoint"
-        })
-    })
+//         if (decoded) {
+//             req.userId = decoded.id;
+//             next()
+//         } else {
+//             res.status(403).json({
+//                 message: "You are not signed in"
+//             })
+//         }    
+//     }
+// }
 
-    module.exports = {
-        adminRouter: adminRouter
+function adminMiddleware(req, res, next) {
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, JWT_ADMIN_PASSWORD);
+
+    if (decoded) {
+        req.userId = decoded.id;
+        next()
+    } else {
+        res.status(403).json({
+            message: "You are not signed in"
+        })
+    }
+
 }
+
+module.exports = {
+    adminMiddleware: adminMiddleware
+}
+// const {Router} = require("express");
+
+// const adminRouter = Router();
+
+// // adminRouter.use(addMiddleware);
+//     adminRouter.post("/signup", function (req, res) {
+//         res.json({
+//             message: "courses endpoint"
+//         })
+//     })
+//     adminRouter.post("/signin", function (req, res) {
+//         res.json({
+//             message: "courses endpoint"
+//         })
+//     })
+//     adminRouter.post("/course", function (req, res) {
+//         res.json({
+//             message: "courses endpoint"
+//         })
+//     })
+
+//     module.exports = {
+//         adminRouter: adminRouter
+// }
